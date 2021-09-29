fd4a187178f6dfc4ca136adc54353a43925725d3
Rachelle Kuhl <alightinmyhair@gmail.com>
2021-09-29 10:09:08 -0500

    Connected to MongoDB which is stored in Config file. Validated the connection as well

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
+const config = require('./config/database');
+
+//Connect to Database
+mongoose.connect(config.database);
+
+//On Connection
+mongoose.connection.on('connected', () => {
+    console.log('Connected to database '+ config.database);
+});
+
+//On Error
+mongoose.connection.on('error', (err) => {
+    console.log('Database error: ' + err);
+});
 
 const app = express();
 
@@ -12,6 +26,9 @@ const users = require('./routes/users');
 const port = 3000;
 
 app.use(cors());
+
+//Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Body Parser Middleware
 app.use(express.json());
