d58eaf4e4b4bc842f4f31837062d78b96ea74dbc
Rachelle Kuhl <alightinmyhair@gmail.com>
2021-09-30 16:43:55 -0500

    Added passport functionality, which will authenticate requests

--- app.js
+++ app.js
@@ -33,6 +33,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 //Body Parser Middleware
 app.use(express.json());
 
+app.use(passport.initialize);
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
 //stating that we want to use /users for all of our user routes
 app.use('/users', users);
 
