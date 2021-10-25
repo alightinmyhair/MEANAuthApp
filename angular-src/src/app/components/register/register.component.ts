a9a025ea13752a23f2dcbfbd91315c11361e517d
Rachelle Kuhl <alightinmyhair@gmail.com>
2021-10-24 23:00:10 -0500

    Added conditional to validate registerUser() via authService

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ValidateService } from 'src/app/services/validate.service';
+import { AuthService } from 'src/app/services/auth.service';
 import { FlashMessagesModule, FlashMessagesService } from 'angular2-flash-messages';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -17,6 +19,8 @@ export class RegisterComponent implements OnInit {
   constructor(
     private validateService: ValidateService,
     private flashMessage: FlashMessagesService,
+    private authService: AuthService,
+    private router: Router
     ) { }
 
   ngOnInit(): void {
@@ -31,6 +35,16 @@ export class RegisterComponent implements OnInit {
       password: this.password
     }
 
+    this.authService.registerUser(user).subscribe(data => {
+      if(data.success) {
+       this.flashMessage.show('You are now registered and can log in', {cssClass: 'alert-success', timeout: 3000});
+       this.router.navigate(['/login']);
+     } else {
+       this.flashMessage.show('Something went wrong', {cssClass: 'alert-danger', timeout: 3000});
+       this.router.navigate(['/register']); 
+     }
+    });
+
     if(!this.validateService.validateRegister(user)) {
       this.flashMessage.show('Please fill in all fields', {cssClass: 'alert-danger', timeout: 3000});
       return false;
@@ -41,5 +55,8 @@ export class RegisterComponent implements OnInit {
       return true;
     }
 
+    
   }
+  
+  
 }
